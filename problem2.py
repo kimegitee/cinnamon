986dbbc1d2bf0f2bce4697a3070edc8cc986177a
kimegitee <kimegitee@gmail.com>
2017-10-02 17:51:48 +0700

    Fix incorrect base case

--- problem2.py
+++ problem2.py
@@ -1,8 +1,8 @@
-def solution(input, depth=500):
+def solution(input):
 	'''Recursively check if a number is "happy"'''
-	if depth <= 0:
+	if input == 4: # 4 will recurse to 4 over and over
 		return False
 	happy = sum([int(n)**2 for n in str(input)])
 	if happy == 1:
 		return True
-	return solution(happy, depth-1)
+	return solution(happy)
