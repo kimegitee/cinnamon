daacf31798a850198e370013a2176680151947e7
kimegitee <kimegitee@gmail.com>
2017-10-02 20:53:06 +0700

    Minor reformat

--- problem2.py
+++ problem2.py
@@ -1,8 +1,10 @@
 def solution(input):
 	'''Recursively check if a number is "happy"'''
-	if input == 4: # 4 will recurse to 4 over and over
-		return False
 	happy = sum([int(n)**2 for n in str(input)])
+
 	if happy == 1:
 		return True
+	if happy == 4: # 4 will recurse to 4 over and over
+		return False
+
 	return solution(happy)
